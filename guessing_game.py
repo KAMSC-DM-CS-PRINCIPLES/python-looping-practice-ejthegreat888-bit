8d5e6ca95ddd0b05f4c5610f47759f397c7b1fcc
ejthegreat888-bit <ej.the.great888@gmail.com>
2025-10-03 12:36:22 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -5,6 +5,16 @@ def guessing_game():
     # HINT: Use input("Enter your guess: ") for user input
     # HINT: Print "Too low! Try again." for low guesses
     # HINT: Print "Too high! Try again." for high guesses
+    while true:
+        inp = input("Enter your guess: ")
+        if int(inp) == 15:
+            print ("Congratulations! You guessed it!")
+            break
+        elif int(inp) > 15:
+            print("Too high! Try again.")
+        else:
+            print("Too low! Try again.")
+
     pass
 
 if __name__ == "__main__":
