2704b1ac0195eca702da4d03f86fe9b50fee29a6
ejthegreat888-bit <ej.the.great888@gmail.com>
2025-10-22 13:39:31 -0400

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -7,7 +7,7 @@ def multiplication_table(n):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             result = i * j
-            print(f"{i} X {j} = {result}")
+            print(f"{i} x {j} = {result}")
 
 if __name__ == "__main__":
     # create multiplication table below this
